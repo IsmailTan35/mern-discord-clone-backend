d9cc297e76eb0835c0ee5f982aa9af10814bc21f
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-12-01 11:47:31 +0300

    vercel

--- index.ts
+++ index.ts
@@ -1,41 +1,41 @@
-// import dotenv from "dotenv";
-// import express from "./backend/app_server/helper/helperExpress";
-// import {
-//     CreateServer,
-//     webSocket
-// } from'./backend/app_server/helper/helperProtocol'
-// import controllerApi from'./backend/app_server/controller/apicontroller'
-// import controlleWebsocket from'./backend/app_server/controller/socketcontroller'
-// import mongoDb from"./backend/app_server/models/mongoDb"
+import dotenv from "dotenv";
+import express from "./backend/app_server/helper/helperExpress";
+import {
+  CreateServer,
+  webSocket,
+} from "./backend/app_server/helper/helperProtocol";
+import controllerApi from "./backend/app_server/controller/apicontroller";
+import controlleWebsocket from "./backend/app_server/controller/socketcontroller";
+import mongoDb from "./backend/app_server/models/mongoDb";
 
-// const env = dotenv.config();
-// const app = express();
-
-// const port = process.env.PORT || 10000
-// const db = mongoDb()
-// const server = CreateServer(app,port)
-// const socket = webSocket(server)
-// app.io=socket
-
-// controllerApi(app)
-// controlleWebsocket(socket,"con")
-
-import express from "express";
-import bodyParser from "body-parser";
-// import cors from "./helperCors"
-// import rateLimit from'express-rate-limit'
-import cors from "cors";
-
+const env = dotenv.config();
 const app = express();
 
-// app.use(cors)
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-// app.use(apiRequestLimiter)
-app.get("/", (req: any, res: any) => {
-  res.send("sadas");
-});
+const port = process.env.PORT || 10000;
+const db = mongoDb();
+const server = CreateServer(app, port);
+const socket = webSocket(server);
+// app.io=socket
 
-app.listen(3000);
+controllerApi(app);
+controlleWebsocket(socket, "con");
+
+// import express from "express";
+// import bodyParser from "body-parser";
+// // import cors from "./helperCors"
+// // import rateLimit from'express-rate-limit'
+// import cors from "cors";
+
+// const app = express();
+
+// // app.use(cors)
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(express.json());
+// // app.use(cors());
+// // app.use(apiRequestLimiter)
+// app.get("/", (req: any, res: any) => {
+//   res.send("sadas");
+// });
+
+// app.listen(3000);
