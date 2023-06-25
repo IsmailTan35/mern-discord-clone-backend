7bf50a654e87b8d23272c155b9a19d1424db5e70
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-06-25 11:25:37 +0300

    actived server

--- backend/app_server/controller/socketcontroller/server/users.ts
+++ backend/app_server/controller/socketcontroller/server/users.ts
@@ -2,68 +2,66 @@ import userSchema from "../../../schema/user";
 import serverSchema from "../../../schema/server";
 import { ObjectId } from "mongodb";
 
-export default async (io:any, socket:any, data:any)=>{
-	const { serverId } = data
-	const token = socket.handshake.auth.token
-	if(!token) return
-	try {
-	const res:any = await userSchema.aggregate([
-		{
-			$match:{
-				token:{
-					$elemMatch:{
-						$eq:token
-					}
-				},
-				servers:{
-					$elemMatch:{$eq:serverId}
-				}
-			},
-		},
-	])
-	const users:any = await serverSchema.aggregate([
-		{
-			$match:{
-				_id:new ObjectId(serverId)
-			}
-		},
-		{
-			$lookup:{
-				from:"discordusers",
-				let:{req:"$userIDs"},
-				pipeline:[
-					{
-						$match:{
-							$expr:{
-								$in:["$_id","$$req"],
-							},
-						}
-					}
-				],
-				as:"users"
-			}
-		},
-		{
-			$unwind:"$users"
-		},
-		{
-			$replaceRoot:{
-				newRoot:"$users"
-			}
-		},
-		{
-			$project:{
-				_id:0,
-				id:"$_id",
-				name:"$username",
-				code:"$code"
-			}
-		}
-
-	])
-	socket.emit("serverUsers",users)
-	} catch (error) {
-		console.error(error)
-
-	}
-}
+export default async (io: any, socket: any, data: any) => {
+  const { serverId } = data;
+  const token = socket.handshake.auth.token;
+  if (!token) return;
+  try {
+    const res: any = await userSchema.aggregate([
+      {
+        $match: {
+          token: {
+            $elemMatch: {
+              $eq: token,
+            },
+          },
+          servers: {
+            $elemMatch: { $eq: serverId },
+          },
+        },
+      },
+    ]);
+    const users: any = await serverSchema.aggregate([
+      {
+        $match: {
+          _id: new ObjectId(serverId),
+        },
+      },
+      {
+        $lookup: {
+          from: "discordusers",
+          let: { req: "$userIDs" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $in: ["$_id", "$$req"],
+                },
+              },
+            },
+          ],
+          as: "users",
+        },
+      },
+      {
+        $unwind: "$users",
+      },
+      {
+        $replaceRoot: {
+          newRoot: "$users",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          id: "$_id",
+          name: "$username",
+          code: "$code",
+        },
+      },
+    ]);
+    socket.emit("serverUsers", users);
+  } catch (error) {
+    console.error(error);
+  }
+};
